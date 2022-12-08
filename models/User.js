da62d6581fc1bcce943f20e7b5711c083e9b0a92
Janaee <janaee.as.wallace@gmail.com>
2022-12-07 21:31:39 -0500

    created profile model for users

--- models/User.js
+++ models/User.js
@@ -1,6 +1,72 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.password);
+    }
+}
 
-Project.init()
+User.init(
+{
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        validate: {
+            isEmail: true,
+        }
+    },
+
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+
+    dateofbirth: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+
+    acctcreatedate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      }
+
+},
+{
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+}
+);
+
+module.exports = User;
