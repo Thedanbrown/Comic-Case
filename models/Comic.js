ec285326a7d76ed287db09abecd73ebf9d72b92b
tylerkandarian <tyler.kand2@gmail.com>
2022-12-12 19:01:32 -0500

    added to the models comic

--- models/Comic.js
+++ models/Comic.js
@@ -1,43 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const createComicData = function (event) {
 
-class Comic extends Model {}
+  const name = document.getElementById('name').value;
+  const issuenum = document.getElementById('issuenum').value;
+  // const volume = document.getElementById('volume').value;
+  // const comicvineid = document.getElementById('comicvineid').value;
+  const pubdat = document.getElementById('coverdate').value;
+  const publisher = document.getElementById('publisher').value;
+  const condition = document.getElementById('condition').value;
+  const image = document.getElementById('imagelink').value;
+  const deck = document.getElementById('deck').value;
+  // const description = document.getElementById('description').value;
 
-Comic.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    //
+// add in additional data elements
+  fetch('/api/comics', {
+      method: 'POST',
+      body: JSON.stringify({
+          title: name,
+          issue: issuenum,
+          // volume: volume,
+          // comicvine_id: comicvineid,
+          published_date: pubdat,
+          publisher: publisher,
+          condition: condition,
+          image: image,
+          deck: deck,
+          // description: description
 
+      })
+  })
+  
+  .then(response => {
+      if (response.status == 200) {
+          alert('You added your comic!')
+      }
+      else {
+          alert('Error, please try again!')
+      }
+  })
 
-    //need to finish code
-
-
-    //
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comic',
-  }
-);
-
-module.exports = Comic;
+}
